df3c4946f5cc66bce065c521fc99f16c7ed94990
Halil Ağın <halil@Halils-MacBook-Pro.local>
2018-07-20 18:23:51 +0100

    screen session created

---
+++ screen-session.sh
@@ -0,0 +1,12 @@
+name=docker
+screen -Sdm $name -t tab0  && \
+screen -S $name -X screen -t tab1 bash && \
+screen -S $name -X screen -t tab2 bash && \
+screen -S $name -X screen -t tab3 bash && \
+screen -S $name -X screen -t tab4 bash && \
+screen -S $name -X screen -t tab5 bash && \
+screen -S $name -X screen -t tab6 bash && \
+screen -S $name -X screen -t tab7 bash && \
+screen -S $name -X screen -t tab8 bash && \
+screen -S $name -X screen -t tab9 bash && \
+screen -r -d $name
